23867e1497329c787f09d78edf96fac18adf4c44
goncalveshigino <goncalveshigino20@gmail.com>
2022-06-07 07:45:59 +0100

    first commit

---
+++ 01-Ola-mundo/app3.js
@@ -0,0 +1,23 @@
+
+console.log('Inicio de programa');
+
+
+
+setTimeout( () => {
+    console.log('primeiro')
+}, 3000);
+
+setTimeout( () => {
+    console.log('Segundo')
+}, 0);
+
+setTimeout( () => {
+    console.log('Terceiro')
+}, 0);
+
+
+
+
+
+
+console.log('Fim de programa')
---
+++ 02-Fundamentos/const-var-let.js
@@ -0,0 +1,30 @@
+
+
+// var nombre = 'Higino';
+
+// if( true ){
+//     var nombre = 'Katiavala'
+// }
+
+// console.log(nombre);
+
+
+
+
+// let nombre = 'Higino';
+
+// if( true ){
+//     nombre = 'Katiavala'
+// }
+
+// console.log(nombre);
+
+
+const nombre = 'Higino';
+
+if( true ){
+     nombre = 'Katiavala'
+}
+
+console.log(nombre);
+
